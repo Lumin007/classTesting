40ceebd3938bfe0037347d592677722cbaffc8a1
Lumin007 <simillion09@gmail.com>
2026-05-28 13:56:03 +0200

    error codes

--- main.cpp
+++ main.cpp
@@ -1,33 +1,43 @@
-
 #include <fstream>
 #include <string>
 #include <chrono>
 #include <iostream>
+#include <exception>
 
 using namespace std;
 
 class Konto {
-    private:
+private:
     string name;
     string nr;
-    double betrag = 0;
+    double betrag;
 
-    public:
-    void konto(string name, string nr) {
+public:
+
+    Konto(string name, string nr) {
         this->name = name;
         this->nr = nr;
+        betrag = 0;
     }
+
     void einzahlen(double money) {
         betrag = betrag + money;
     }
+
     void auszahlen(double money) {
+        if (money > betrag) {
+            throw runtime_error("Fehler: Nicht genug Geld auf dem Konto!");
+        }
         betrag = betrag - money;
     }
+
     void ueberweisen(Konto& zielkonto, double money) {
+        if (money > betrag) {
+            throw runtime_error("Fehler: Nicht genug Geld auf dem Konto!");
+        }
         this->auszahlen(money);
         zielkonto.einzahlen(money);
     }
-
 
     void kontostand_anzeigen() {
         cout << name << " (" << nr << "): " << betrag << " Euro" << endl;
@@ -35,12 +45,23 @@ class Konto {
 };
 
 int main() {
-    Konto *k1 = new Konto;
-    Konto *k2 = new Konto;
-    k1->einzahlen(200);
-    k2->einzahlen(200);
-    k1->auszahlen(50);
-    k1->ueberweisen(*k2, 100);
-    k1->ueberweisen(*k2, 250);
-    k2->auszahlen(500);
+    Konto k1("BOB", "1");
+    Konto k2("ALICE", "2");
+
+    try {
+        k1.einzahlen(200);
+        k2.einzahlen(200);
+        k1.auszahlen(50);
+        k1.ueberweisen(k2, 100);
+        k1.kontostand_anzeigen();
+        k2.kontostand_anzeigen();
+        k1.ueberweisen(k2, 250);
+        k2.auszahlen(500);
+
+    } catch (const exception& e) {
+
+        cerr << "\nError: " << e.what() << endl;
+    }
+
+    return 0;
 }
