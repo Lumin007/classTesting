383cc51d71d6b6656ea3247877b8731970f8ec64
Lumin007 <simillion09@gmail.com>
2026-05-28 13:29:54 +0200

    verson 1 push

--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
-#include <optional>
-#include <cstdint>
+
 #include <fstream>
 #include <string>
 #include <chrono>
-#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Konto {
+    private:
+    string name;
+    string nr;
+    double betrag = 0;
+
+    public:
+    void konto(string name, string nr) {
+        this->name = name;
+        this->nr = nr;
+    }
+    void einzahlen(double money) {
+        betrag = betrag + money;
+    }
+    void auszahlen(double money) {
+        betrag = betrag - money;
+    }
+    void ueberweisen(Konto& zielkonto, double money) {
+        this->auszahlen(money);
+        zielkonto.einzahlen(money);
+    }
+
+
+    void kontostand_anzeigen() {
+        cout << name << " (" << nr << "): " << betrag << " Euro" << endl;
+    }
+};
 
 int main() {
-
-    return 0;
+    Konto *k1 = new Konto;
+    Konto *k2 = new Konto;
+    k1->einzahlen(200);
+    k2->einzahlen(200);
+    k1->auszahlen(50);
+    k1->ueberweisen(*k2, 100);
+    k1->ueberweisen(*k2, 250);
+    k2->auszahlen(500);
 }
